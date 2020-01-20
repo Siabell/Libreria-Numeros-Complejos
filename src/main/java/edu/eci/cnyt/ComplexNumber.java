830716bd424fcf426a2e02344332f7fc2016e2f9
Siabell <valesiabatto@gmail.com>
2020-01-19 23:59:11 -0500

    pruebas funciones numero complejos

--- src/main/java/edu/eci/cnyt/ComplexNumber.java
+++ src/main/java/edu/eci/cnyt/ComplexNumber.java
@@ -10,25 +10,39 @@ import java.math.*;
  *
  * @author valentina Siabatto
  */
-public class Complex  {
+public class ComplexNumber  {
     
    private double parteReal;
    private double parteImaginaria;
-
-    public Complex(double a1, double b1) {
+   
+   /**
+    * Constructor de la clase numeros complejos
+    * @param a1: parte real
+    * @param b1: parte imaginaria
+    */
+    public ComplexNumber(double a1, double b1) {
         parteReal=a1;
         parteImaginaria=b1;
     }
     
     public static void main(String args[]){
         System.out.println("gdrgdrh");
-        Complex a = new Complex(-2,1);
-        Complex b = new Complex(1,2);
+        ComplexNumber a = new ComplexNumber(-2,1);
+        ComplexNumber b = new ComplexNumber(1,2);
         //a.division(b);
-        Complex c = new Complex(1,-1);
-        System.out.println(c.modulus());
-        //System.out.println(a.add(b).getParteImaginaria());
-         
+        ComplexNumber c = new ComplexNumber(1,-1);
+        
+        ComplexNumber c1 = new ComplexNumber(3, -1);
+		ComplexNumber c2 = new ComplexNumber(1, 4);
+		ComplexNumber cAddResultTest = c1.add(c2);
+		ComplexNumber cAddResult = new ComplexNumber(4, 3);
+        System.out.println(c1.add(c2));
+        System.out.println(c1.add(c2).getParteImaginaria());
+        System.out.println(c1.add(c2).getParteReal());
+        
+        cAddResult.Equals(cAddResultTest);
+        
+        
     }
 
     public double getParteReal() {
@@ -39,34 +53,34 @@ public class Complex  {
         return parteImaginaria;
     }
     
-    public Complex add (Complex parteAdd){
-       double real= parteReal+parteAdd.getParteReal();
-       double imaginaria= parteImaginaria+parteAdd.getParteImaginaria();
-       Complex nuevoComplex = new Complex(real,imaginaria);
+    public ComplexNumber add (ComplexNumber partAdd){
+       double real= parteReal+partAdd.getParteReal();
+       double imaginaria= parteImaginaria+partAdd.getParteImaginaria();
+       ComplexNumber nuevoComplex = new ComplexNumber(real,imaginaria);
        return nuevoComplex;   
     }
     
-    public Complex multiplication (Complex parteAdd){
+    public ComplexNumber multiplication (ComplexNumber parteAdd){
        double real= (parteReal*parteAdd.getParteReal())-(parteImaginaria*parteAdd.getParteImaginaria());
        double imaginaria= (parteReal*parteAdd.getParteImaginaria())+(parteAdd.getParteReal()*parteImaginaria);
-       Complex nuevoComplex = new Complex(real,imaginaria);
+       ComplexNumber nuevoComplex = new ComplexNumber(real,imaginaria);
        return nuevoComplex;   
     }
     
-    public Complex substract (Complex parteAdd){
+    public ComplexNumber substract (ComplexNumber parteAdd){
        double real= parteReal-parteAdd.getParteReal();
        double imaginaria= parteImaginaria-parteAdd.getParteImaginaria();
-       Complex nuevoComplex = new Complex(real,imaginaria);
+       ComplexNumber nuevoComplex = new ComplexNumber(real,imaginaria);
        return nuevoComplex;   
     }
     
-    public Complex division (Complex parteAdd){
+    public ComplexNumber division (ComplexNumber parteAdd){
        double real= (parteReal*parteAdd.getParteReal())+(parteImaginaria*parteAdd.getParteImaginaria());
        double x = real/( Math.pow(parteAdd.getParteReal(), 2)+Math.pow(parteAdd.getParteImaginaria(), 2));
        double imaginaria= (parteAdd.getParteReal()*parteImaginaria)-(parteReal*parteAdd.getParteImaginaria());
        double y = imaginaria/( Math.pow(parteAdd.getParteReal(), 2)+Math.pow(parteAdd.getParteImaginaria(), 2));
        
-       Complex nuevoComplex = new Complex(x,y);
+       ComplexNumber nuevoComplex = new ComplexNumber(x,y);
        return nuevoComplex;   
     }
     
@@ -77,22 +91,28 @@ public class Complex  {
        return modulo; 
     }
     
-    public Complex comjugate (){
-       Complex nuevoComplex = new Complex(this.getParteReal(),(this.getParteImaginaria()*-1));
+    public ComplexNumber conjugate (){
+    	ComplexNumber nuevoComplex = new ComplexNumber(this.getParteReal(),(this.getParteImaginaria()*-1));
        return nuevoComplex; 
     }
     
-    //funcion atan es arc tangente
-    public Complex polarRepresentation (){
+    
+    public ComplexNumber polarRepresentation (){
        double p = Math.sqrt(Math.pow(parteReal, 2)+Math.pow(parteImaginaria, 2));
        double o = Math.atan(this.getParteReal()/this.getParteImaginaria());
-       Complex nuevoComplex = new Complex(p,o);
+       ComplexNumber nuevoComplex = new ComplexNumber(p,o);
        return nuevoComplex; 
     }
     
+    public double phase() {
+    	double phaseOfNumber = Math.atan(this.getParteReal()/this.getParteImaginaria());
+		return phaseOfNumber;
+    	
+    }
     
-    public boolean Equals(Complex otherComplex) {
+    public boolean Equals(ComplexNumber otherComplex) {
     	boolean result = this.getParteReal()==otherComplex.getParteReal() && this.getParteImaginaria()==otherComplex.getParteImaginaria();
+    	System.out.println(result);
 		return result;
     	
     }
